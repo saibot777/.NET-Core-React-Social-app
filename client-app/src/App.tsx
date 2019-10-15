cb2f346af61513b333d77cbff91cad5beb261166
saibot777 <stefantrajkovic777@gmail.com>
2019-10-15 15:06:15 +0200

    initiating client app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    values: []
+  }
+
+  componentDidMount() {
+    this.setState({
+      values: [{id: 1, name: 'Value 101'}, {id: 2, name: 'Value 102'}]
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Reactivities</h1>
+        <ul>
+          {this.state.values.map((value: any) => (
+            <li>{value.name}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
