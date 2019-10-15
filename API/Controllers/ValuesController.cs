3cc8e5b862b1b4dcf879ea5bda35b0ff2639e6c3
saibot777 <stefantrajkovic777@gmail.com>
2019-10-15 14:21:21 +0200

    get value by id endpoint ready

--- API/Controllers/ValuesController.cs
+++ API/Controllers/ValuesController.cs
@@ -30,9 +30,10 @@ namespace API.Controllers
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task <ActionResult<Value>> Get(int id)
         {
-            return "value";
+            var value = await _context.Values.FindAsync(id);
+            return Ok(value);
         }
 
         // POST api/values
